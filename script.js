ec3e7a805c5a206a0fa59d4589faf2fdc6928b36
admin <admin@iMac.fritz.box>
2026-06-09 11:05:49 +0200

    Startseite erstellt mit Grundlayout

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("#menu-button");
+
+const menu = document.querySelector("#menu");
+
+button.addEventListener("click", function () {
+
+  menu.classList.toggle("hidden");
+
+});
